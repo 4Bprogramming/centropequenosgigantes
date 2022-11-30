ca3d7f87d6ebf99af9a4cde90df812c143f2dc40
Neleon10 <cvn45@hotmail.com>
2022-11-30 19:28:27 -0300

    acomodando textos

--- src/Components/Home/Home2.jsx
+++ src/Components/Home/Home2.jsx
@@ -1,27 +1,29 @@
-import React from 'react'
+import React from "react";
 import styles from "./Home2.module.css";
 import pict from "../../assets/pequenos_gigantes.jpg";
-
 
 function Home2() {
   return (
     <div className={styles.AboutBody}>
-       
-        
-        <div className={styles.AboutContainer}>
-          <h1 className={styles.StyledHeading}>¡BIENVENIDOS!</h1>
-          <div className={styles.TextSection} style={{textAlign:"justify"}}>
-          Bienvenido a “Pequeños Gigantes” Nos especializamos en brindar atención a 
-          pacientes con diversos diagnósticos contamos con profesionales tecnólogos médicos 
-          y psicólogos los cuales tienen por misión brindar la mejor atención de calidad. 
-           
-          </div>
+
+      <div className={styles.AboutContainer}>
+        <h1 className={styles.StyledHeading}>¡BIENVENIDOS!</h1>
+
+        <div className={styles.TextSection}>
+          Bienvenido a “Pequeños Gigantes” Nos especializamos en brindar
+          atención a pacientes con diversos diagnósticos contamos con
+          profesionales tecnólogos médicos y psicólogos los cuales tienen por
+          misión brindar la mejor atención de calidad.
         </div>
-        <div className={styles.AboutContainer}>
-          <img className={styles.Photo} src={pict} />
-        </div>
+
       </div>
-  )
+
+      <div className={styles.PhotoContainer}>
+        
+      </div>
+
+    </div>
+  );
 }
 
-export default Home2
+export default Home2;
