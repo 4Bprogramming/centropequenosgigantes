e043b3ad90fd638530dc13389a77bc297a9b510d
gabdala17 <gisella.abdala@gmail.com>
2022-12-03 11:15:47 -0300

    constantes arreglo

--- src/Redux/constants.js
+++ src/Redux/constants.js
@@ -1,8 +1,8 @@
-export const GET_TURNOS= get_turnos
-export const GET_USUARIOS= get_usuarios
-export const GET_PROFESIONALES=get_profesionales
-export const GET_ADMIN= get_admin
-export const POST_TURNOS= post_turnos
-export const POST_USUARIOS= post_usuarios
-export const POST_PROFESIONALES= post_profesionales
-export const POST_ADMIN= post_admin
+export const GET_TURNOS= 'get_turnos'
+export const GET_USUARIOS= 'get_usuarios'
+export const GET_PROFESIONALES='get_profesionales'
+export const GET_ADMIN= 'get_admin'
+export const POST_TURNOS= 'post_turnos'
+export const POST_USUARIOS= 'post_usuarios'
+export const POST_PROFESIONALES= 'post_profesionales'
+export const POST_ADMIN= 'post_admin'
